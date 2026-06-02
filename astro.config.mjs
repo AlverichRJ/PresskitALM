07e762d25210225449efe68671471f3a283303ee
AlverichRJ <2.5267482e+07+AlverichRJ@users.noreply.github.com>
2026-06-02 20:59:17 +0000

    chore(config): soporta ruta de revision en github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 export default defineConfig({
   output: 'static',
+  site: isGitHubPages ? 'https://alverichrj.github.io' : undefined,
+  base: isGitHubPages ? '/PresskitALM' : '/',
   integrations: [react(), tailwind({ applyBaseStyles: false })],
   build: {
     format: 'directory',
